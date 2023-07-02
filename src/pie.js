963bc6401dea39a57faa3e43c282526695deb4fe
Deepak Vishwakarma <90350741+Deepakv2104@users.noreply.github.com>
2023-07-03 03:41:53 +0530

    Update pie.js

--- src/pie.js
+++ src/pie.js
@@ -1,47 +1,42 @@
-/* App.js */
-import React, { Component } from "react";
+import React from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
-//var CanvasJSReact = require('@canvasjs/react-charts');
 import "./pie.css";
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class Pie extends Component {
-  render() {
-    const options = {
-      exportEnabled: true,
-      animationEnabled: true,
-      title: {
-        text: "Website Traffic Sources"
-      },
-      data: [
-        {
-          type: "pie",
-          startAngle: 75,
-          toolTipContent: "<b>{label}</b>: {y}%",
-          showInLegend: "true",
-          legendText: "{label}",
-          indexLabelFontSize: 16,
-          indexLabel: "{label} - {y}%",
-          dataPoints: [
-            { y: 18, label: "Direct" },
-            { y: 49, label: "Organic Search" },
-            { y: 9, label: "Paid Search" },
-            { y: 5, label: "Referral" },
-            { y: 19, label: "Social" }
-          ]
-        }
-      ]
-    };
-    return (
-      <div className="pie">
-        <CanvasJSChart
-          options={options}
-          /* onRef={ref => this.chart = ref} */
-        />
-        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-      </div>
-    );
-  }
-}
-module.exports = Pie;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const Pie = () => {
+  const options = {
+    exportEnabled: true,
+    animationEnabled: true,
+    title: {
+      text: "Website Traffic Sources"
+    },
+    data: [
+      {
+        type: "pie",
+        startAngle: 75,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 18, label: "Direct" },
+          { y: 49, label: "Organic Search" },
+          { y: 9, label: "Paid Search" },
+          { y: 5, label: "Referral" },
+          { y: 19, label: "Social" }
+        ]
+      }
+    ]
+  };
+
+  return (
+    <div className="pie">
+      <CanvasJSChart options={options} />
+      {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+    </div>
+  );
+};
+
+export default Pie;
